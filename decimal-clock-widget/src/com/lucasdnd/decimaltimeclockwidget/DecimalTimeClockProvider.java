0b82859357e7452cbd64b3a2fffebc4766a6e4f4
Tulio <tulio@doubleleft.com>
2014-09-12 19:48:30 -0300

    Widget layout

--- decimal-clock-widget/src/com/lucasdnd/decimaltimeclockwidget/DecimalTimeClockProvider.java
+++ decimal-clock-widget/src/com/lucasdnd/decimaltimeclockwidget/DecimalTimeClockProvider.java
@@ -9,12 +9,20 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.text.format.Time;
 import android.widget.RemoteViews;
 
 public class DecimalTimeClockProvider extends AppWidgetProvider {
 	
 	public static String CLOCK_UPDATE = "com.lucasdnd.decimaltimeclockwidget.CLOCK_UPDATE";
+	private static int canvasSize = 400;
+	private static int canvasPadding = 12;
 	
 	@Override
 	public void onReceive(Context context, Intent intent) {
@@ -85,7 +93,7 @@ public class DecimalTimeClockProvider extends AppWidgetProvider {
 	public static void updateClock(Context context,	AppWidgetManager appWidgetManager, int appWidgetId) {
 		
 		// Update the time text
-		RemoteViews remoteViews = new RemoteViews(context.getPackageName(),	R.layout.widgetlayout);
+		RemoteViews views = new RemoteViews(context.getPackageName(),	R.layout.widgetlayout);
 		
 		Time today = new Time(Time.getCurrentTimezone());
 		today.setToNow();
@@ -93,15 +101,40 @@ public class DecimalTimeClockProvider extends AppWidgetProvider {
 		int minute = today.minute;
 		int second = today.second;
 		double time = second + (minute * 60) + (hour * 60 * 60);
-		double total = 86400; // Seconds in a day
-		double decimalHour = time * 100 / total;
+		double secondsInDay = 86400; // Seconds in a day
+		double decimalHour = time * 100 / secondsInDay;
 		int decimalMinuteInt = (int)(Math.floor((decimalHour - Math.floor(decimalHour)) * 100));
 		String decimalMinute = "" + decimalMinuteInt;
 		if(decimalMinute.length() == 1) {
 			decimalMinute =  "0" + decimalMinute;
 		}
+		float timeSlice = (float)(time * 360 / secondsInDay);
 		
-		remoteViews.setTextViewText(R.id.clockTextView, "" + ((int)decimalHour + ":" + decimalMinute));
-		appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+		Bitmap bitmap = Bitmap.createBitmap(canvasSize, canvasSize, Config.ARGB_8888);
+		Canvas canvas = new Canvas(bitmap);
+		Paint p = new Paint();
+		p.setAntiAlias(true);
+		
+		// Draw the white background
+		p.setColor(Color.argb(128, 255, 255, 255));
+		p.setStyle(Paint.Style.FILL);
+		canvas.drawCircle(canvasSize / 2, canvasSize / 2, canvasSize / 2 - canvasPadding, p);
+		
+		// Draw the gray circle
+		p.setColor(Color.argb(255, 200, 200, 200));
+		p.setStyle(Paint.Style.STROKE);
+		p.setStrokeWidth(3);
+		canvas.drawCircle(canvasSize / 2, canvasSize / 2, canvasSize / 2 - canvasPadding, p);
+		
+		// Draw the white arc
+		p.setColor(Color.argb(255, 255, 255, 255));
+		p.setStyle(Paint.Style.STROKE);
+		p.setStrokeWidth(canvasPadding);
+		p.setStrokeCap(Paint.Cap.ROUND);
+		canvas.drawArc(new RectF(canvasPadding, canvasPadding, canvasSize - canvasPadding, canvasSize - canvasPadding), -90f, timeSlice, false, p);
+		
+		views.setImageViewBitmap(R.id.background, bitmap);
+		views.setTextViewText(R.id.clockTextView, "" + ((int)decimalHour + ":" + decimalMinute));
+		appWidgetManager.updateAppWidget(appWidgetId, views);
 	}
 }
