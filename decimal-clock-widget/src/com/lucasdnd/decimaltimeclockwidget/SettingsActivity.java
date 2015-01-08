6e6bf51084b074ee6f131d1d83b77b2696e24c1c
Tulio <tulio@doubleleft.com>
2015-01-08 19:31:45 -0200

    Added basic settings controls

--- decimal-clock-widget/src/com/lucasdnd/decimaltimeclockwidget/SettingsActivity.java
+++ decimal-clock-widget/src/com/lucasdnd/decimaltimeclockwidget/SettingsActivity.java
@@ -1,7 +1,14 @@
 package com.lucasdnd.decimaltimeclockwidget;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class SettingsActivity extends Activity {
-
+	
+	@Override
+	public void onCreate(Bundle savedInstance) {
+		super.onCreate(savedInstance);
+		
+		this.setContentView(R.layout.settings_activity);
+	}
 }
