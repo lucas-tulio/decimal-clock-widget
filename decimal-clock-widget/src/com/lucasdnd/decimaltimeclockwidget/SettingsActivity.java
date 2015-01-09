91f00083b873420c96a341165bd1e1d6041d8a93
Tulio <tulio@doubleleft.com>
2015-01-09 16:24:53 -0200

    Settings spinner setup

--- decimal-clock-widget/src/com/lucasdnd/decimaltimeclockwidget/SettingsActivity.java
+++ decimal-clock-widget/src/com/lucasdnd/decimaltimeclockwidget/SettingsActivity.java
@@ -2,6 +2,9 @@ package com.lucasdnd.decimaltimeclockwidget;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SettingsActivity extends Activity {
 	
@@ -10,5 +13,20 @@ public class SettingsActivity extends Activity {
 		super.onCreate(savedInstance);
 		
 		this.setContentView(R.layout.settings_activity);
+		
+		// Day and month spinner
+		Spinner dayMonthSpinner = (Spinner) findViewById(R.id.dayMonthSpinner);
+		
+		String[] dayMonthOptions = {"Gregorian Calendar", "World Season Calendar"};
+		ArrayAdapter<String> dayMonthAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, dayMonthOptions);
+		dayMonthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		dayMonthSpinner.setAdapter(dayMonthAdapter);
+		
+		// Year Input
+		Time today = new Time(Time.getCurrentTimezone());
+		today.setToNow();
+		
+		// 1543
+		
 	}
 }
